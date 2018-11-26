2c31a80f74be9e81b74655b35a31f8549434559e
vidyashree <41608378+vidyashhraj55@users.noreply.github.com>
2018-11-26 18:20:43 +0530

    Update ScheduledTasks.java

--- src/main/java/com/example/demo/ScheduledTasks.java
+++ src/main/java/com/example/demo/ScheduledTasks.java
@@ -16,16 +16,22 @@ public class ScheduledTasks {
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-
+	
+//rus every 1 seconds
     @Scheduled(fixedRate = 1000)
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
     }
+	
+	
+	//runs every second but Wait 2 seconds before it exceute for the first time
     @Scheduled(initialDelay=2000,fixedRate = 1000)
     public void scheduleFixedRateTask() {
         System.out.println(
           "Fixed rate task - " + System.currentTimeMillis() / 1000);
     }
+	
+	//runs every 5 seconds
 	@Scheduled(cron = "*/5 * * * * *")
     public void scheduleTaskUsingCronExpression() {
     long now = System.currentTimeMillis() / 1000;
